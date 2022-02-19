e53488beecb025a2d77d08c660b4e7a1c7a50110
sujeetgh <sujeetmehta6@gmail.com>
2022-02-19 09:01:47 +0530

    Updated install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ printf "Script started!\n"
 pkg install neovim -y
 pkg install python -y
 pkg install git -y
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 
 if [ ! -d ".config" ]; then
 	mkdir ".config"
