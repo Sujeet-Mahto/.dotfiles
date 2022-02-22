de8736c6bfa7a9c97362ee8fcf4185872e754817
sujeetgh <sujeetmehta6@gmail.com>
2022-02-22 19:51:48 +0530

    added htmldjando in html lsp

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -12,9 +12,15 @@ map("n", "nn", ":tabnext<CR>")
 map("n", "<F9>", ":w !python<CR>")
 map("n", "tt", ":NvimTreeToggle<CR>")
 map("i", "jk", "<ESC>")
-map("i", "JK", "<ESC>")
-map("i", "jK", "<ESC>")
-map("i", "Jk", "<ESC>")
+map("i", "kj", "<ESC>")
 map("n", "<C-S>", ":w<CR>")
 map("i", "<C-S>", "<ESC>:w<CR>")
+map("n", "<C-Q>", ":q!<CR>")
+
+
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
