1a2e81da92f1ef5d55209b3f7d2e107817cff88a
sujeetgh <sujeetmehta6@gmail.com>
2022-03-09 20:46:01 +0530

    resetting termux

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,9 +6,12 @@ function map(mode, lhs, rhs, opts)
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+local opts = { noremap=true, silent=true }
+
 -- Mappings
 map("n", ",<Space>", ":nohlsearch<CR>", { silent = true })
-map("n", "nn", ":tabnext<CR>")
+map("n", "<tab>", ":tabnext<CR>")
+map("n", "<S-<tab>>", ":tabprevious")
 map("n", "<F9>", ":w !python<CR>")
 map("n", "tt", ":NvimTreeToggle<CR>")
 map("i", "jf", "<ESC>")
@@ -18,7 +21,6 @@ map("i", "<C-S>", "<ESC>:w<CR>")
 map("n", "<C-Q>", ":q!<CR>")
 
 
-local opts = { noremap=true, silent=true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
