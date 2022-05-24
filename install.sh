e5331316fb162a886117f1c7fa227d1cc571c646
sujeetgh <sujeetmehta6@gmail.com>
2022-05-24 21:44:23 +0530

    added lunarvim installation

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 cd $HOME
+
+printf "Upgrading Packages."
 apt update && apt upgrade
 
+printf "Installing required packages."
 apt install git -y
 apt install curl -y
 apt install zsh -y
@@ -10,20 +13,29 @@ apt install python -y
 apt install neovim -y
 apt install nodejs -y
 apt install tree -y
+apt install lua-language-server -y
 
 pip install --upgrade pip
 pip install wheel
 
 # Lunarvim setup
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+
+if [ ! -d ~/.config/lvim ]
+then
+  mkdir ~/.config/lvim
+fi
+ln -sf ~/.dotfiles/lvim/config.lua ~/.config/lvim/config.lua
+
 mv .local/bin/lvim ../usr/bin/
 
-# ZSH setup
+# # ZSH setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
 # chsh -s zsh
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 git clone https://github.com/adi1090x/termux-style
 cd termux-style
 ./install
 ./tstyle
 
-
+printf "\n\nScript complete. Restart terminal.\n"
